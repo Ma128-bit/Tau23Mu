adf5a3655a320150333a61a6adb353cd1342634d
Marco Buonsante <marco.buonsante@cern.ch>
2023-08-21 11:03:55 +0200

    run C 2023

--- CrabSubmission/Data_2023/SkimPhiPi/reco2023_RunC-v1/crab_DsPhiPi_reco2023_RunC-v1_stream6_cfg.py
+++ CrabSubmission/Data_2023/SkimPhiPi/reco2023_RunC-v1/crab_DsPhiPi_reco2023_RunC-v1_stream6_cfg.py
@@ -8,14 +8,14 @@ config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
 
-config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/CMSSW_12_4_11_patch3/src/SkimTools/SkimPhiPi/test/run_Data2023C-v1-3_DsPhiPiSkimAndTree_cfg.py'
+config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/CMSSW_13_0_10/src/SkimTools/SkimPhiPi/test/run_Data2023C-v1-3_DsPhiPiSkimAndTree_cfg.py'
 
 config.Data.inputDataset = '/ParkingDoubleMuonLowMass6/Run2023C-PromptReco-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 #config.Data.splitting = 'Automatic'
 config.Data.unitsPerJob = 50
-config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions23/Cert_Collisions2023_eraC_367095_368224_Golden.json'
+config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions23/Cert_Collisions2023_eraC_367095_368823_Golden.json'
 #config.Data.runRange = '193093-193999' # '193093-194075'
 #config.Data.publication = True
 config.Data.outputDatasetTag = 'SkimDsPhiPi_2023eraC-v1_stream6_Mini_v3'
