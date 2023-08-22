2907c55be28be746be01f220402e39ae58131c6e
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-08-22 14:28:29 +0200

    Update crab_Tau3mu_reco2023_RunD-v2_stream1_cfg.py

--- CrabSubmission/Data_2023/SkimTau3mu/reco2023_RunD-v2/crab_Tau3mu_reco2023_RunD-v2_stream1_cfg.py
+++ CrabSubmission/Data_2023/SkimTau3mu/reco2023_RunD-v2/crab_Tau3mu_reco2023_RunD-v2_stream1_cfg.py
@@ -11,7 +11,7 @@ config.JobType.pluginName = 'Analysis'
 
 config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/CMSSW_13_0_10/src/SkimTools/SkimTau3Mu/test/run_Data2023D_PatAndTree_cfg.py'
 
-config.Data.inputDataset = '/ParkingDoubleMuonLowMass1/Run2022D-10Dec2022-v3/MINIAOD'
+config.Data.inputDataset = '/ParkingDoubleMuonLowMass1/Run2023D-PromptReco-v2/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 #config.Data.splitting = 'Automatic'
