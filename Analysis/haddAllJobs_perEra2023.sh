282756189014b03375ebd094627ea0175c73d2a5
Marco Buonsante <marco.buonsante@cern.ch>
2023-08-23 13:01:36 +0200

    2023

--- Analysis/haddAllJobs_perEra2023.sh
+++ Analysis/haddAllJobs_perEra2023.sh
@@ -1,4 +1,4 @@
-for era in B C-v1 C-v2 C-v3 C-v4
+for era in B C-v1 C-v2 C-v3 C-v4 D-v1 D-v2
 do
-	hadd JobAdd_perEra/Era_${era}_control.root 2023${era}_*_control_ReReco/AnalysedTree_data_control_2023${era}_*_control_merged_ReReco.root
+	hadd JobAdd_perEra/Era_${era}_control.root 2023${era}_*_control_PromptReco/AnalysedTree_data_control_2023${era}_*_control_merged_PromptReco.root
 done
