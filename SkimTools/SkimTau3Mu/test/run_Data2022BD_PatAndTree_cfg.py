97c936f0613d6822a44438d85c8aaf9850f7dc5c
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-02 17:13:49 +0100

    Update run_Data2022BD_PatAndTree_cfg.py

--- SkimTools/SkimTau3Mu/test/run_Data2022BD_PatAndTree_cfg.py
+++ SkimTools/SkimTau3Mu/test/run_Data2022BD_PatAndTree_cfg.py
@@ -13,8 +13,8 @@ process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
-#process.load("SkimTools.SkimTau3Mu.Tau3MuSkim_miniAOD_cff")
-process.load("SkimTools.SkimTau3Mu.Tau3MuSkim_miniAOD_noHLT_cff")
+process.load("SkimTools.SkimTau3Mu.Tau3MuSkim_miniAOD_cff")
+#process.load("SkimTools.SkimTau3Mu.Tau3MuSkim_miniAOD_noHLT_cff")
 
 #process.GlobalTag.globaltag = '94X_mc2017_realistic_v14'
 #process.GlobalTag.globaltag = '102X_dataRun2_Prompt_v16' #data2018D
