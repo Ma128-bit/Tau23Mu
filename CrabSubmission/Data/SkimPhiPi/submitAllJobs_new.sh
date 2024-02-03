070ea9de52dcbf7c99cdd2af69374e48c9ce5148
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-04 00:24:42 +0100

    Update submitAllJobs_new.sh

--- CrabSubmission/Data/SkimPhiPi/submitAllJobs_new.sh
+++ CrabSubmission/Data/SkimPhiPi/submitAllJobs_new.sh
@@ -2,7 +2,7 @@
 # Usage:
 #    reportAllJobs.sh
 
-for i in 2022_RunC 2022_RunD 2022_RunE 2022_RunF 2022_RunG
+for i in 2022_RunC 2022_RunD-v1 2022_RunD-v2 2022_RunE 2022_RunF 2022_RunG
 do
     echo -e "\nData $i"
     cd reco${i}
