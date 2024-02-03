c00b41bfd3316a0b4352549e34fd410d87cfa023
Marco Buonsante <marco.buonsante@cern.ch>
2024-02-04 00:40:38 +0100

    Prompt D

--- CrabSubmission/Data/SkimTau3mu/reco2022_RunD-v1/crab_Tau3mu_reco2022_RunD-v1_stream2_cfg.py
+++ CrabSubmission/Data/SkimTau3mu/reco2022_RunD-v1/crab_Tau3mu_reco2022_RunD-v1_stream2_cfg.py
@@ -1,17 +1,16 @@
 from CRABClient.UserUtilities import config, getUsername
 config = config()
 
-config.General.requestName = 'SkimTau3mu_2022eraD_stream0_Mini_v3'
+config.General.requestName = 'SkimTau3mu_2022eraD-v1_stream2_Mini_v3'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
 
-
 config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/SkimTools/SkimTau3Mu/test/run_Data2022BD_PatAndTree_cfg.py'
 
-config.Data.inputDataset = '/ParkingDoubleMuonLowMass0/Run2022D-10Dec2022-v2/MINIAOD'
+config.Data.inputDataset = '/ParkingDoubleMuonLowMass2/Run2022D-PromptReco-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 #config.Data.splitting = 'Automatic'
@@ -19,7 +18,7 @@ config.Data.unitsPerJob = 50
 config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions22/Cert_Collisions2022_eraD_357538_357900_Golden.json'
 #config.Data.runRange = '193093-193999' # '193093-194075'
 #config.Data.publication = True
-config.Data.outputDatasetTag = 'SkimTau3mu_2022eraD_stream0_Mini_v3'
+config.Data.outputDatasetTag = 'SkimTau3mu_2022eraD-v1_stream2_Mini_v3'
 config.JobType.allowUndistributedCMSSW = True 
 config.Site.storageSite = 'T2_IT_Bari'
 config.Site.ignoreGlobalBlacklist  = True
--- CrabSubmission/Data/SkimTau3mu/reco2022_RunD-v2/crab_Tau3mu_reco2022_RunD-v2_stream2_cfg.py
+++ CrabSubmission/Data/SkimTau3mu/reco2022_RunD-v2/crab_Tau3mu_reco2022_RunD-v2_stream2_cfg.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsername
 config = config()
 
-config.General.requestName = 'SkimTau3mu_2022eraD_stream7_Mini_v3'
+config.General.requestName = 'SkimTau3mu_2022eraD-v2_stream2_Mini_v3'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -10,7 +10,7 @@ config.JobType.pluginName = 'Analysis'
 
 config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/SkimTools/SkimTau3Mu/test/run_Data2022BD_PatAndTree_cfg.py'
 
-config.Data.inputDataset = '/ParkingDoubleMuonLowMass7/Run2022D-10Dec2022-v2/MINIAOD'
+config.Data.inputDataset = '/ParkingDoubleMuonLowMass2/Run2022D-PromptReco-v2/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 #config.Data.splitting = 'Automatic'
@@ -18,7 +18,7 @@ config.Data.unitsPerJob = 50
 config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions22/Cert_Collisions2022_eraD_357538_357900_Golden.json'
 #config.Data.runRange = '193093-193999' # '193093-194075'
 #config.Data.publication = True
-config.Data.outputDatasetTag = 'SkimTau3mu_2022eraD_stream7_Mini_v3'
+config.Data.outputDatasetTag = 'SkimTau3mu_2022eraD-v2_stream2_Mini_v3'
 config.JobType.allowUndistributedCMSSW = True 
 config.Site.storageSite = 'T2_IT_Bari'
 config.Site.ignoreGlobalBlacklist  = True
