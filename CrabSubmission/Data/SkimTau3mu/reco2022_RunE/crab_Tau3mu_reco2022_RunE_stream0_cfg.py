c4761f5fc12d5fc17fda1ac56be209ac9e8197ca
Marco Buonsante <marco.buonsante@cern.ch>
2024-02-04 00:44:05 +0100

    Prompt E

--- CrabSubmission/Data/SkimTau3mu/reco2022_RunE/crab_Tau3mu_reco2022_RunE_stream0_cfg.py
+++ CrabSubmission/Data/SkimTau3mu/reco2022_RunE/crab_Tau3mu_reco2022_RunE_stream0_cfg.py
@@ -11,7 +11,7 @@ config.JobType.pluginName = 'Analysis'
 
 config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/SkimTools/SkimTau3Mu/test/run_Data2022E_PatAndTree_cfg.py'
 
-config.Data.inputDataset = '/ParkingDoubleMuonLowMass0/Run2022E-10Dec2022-v2/MINIAOD'
+config.Data.inputDataset = '/ParkingDoubleMuonLowMass0/Run2022E-PromptReco-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 #config.Data.splitting = 'Automatic'
