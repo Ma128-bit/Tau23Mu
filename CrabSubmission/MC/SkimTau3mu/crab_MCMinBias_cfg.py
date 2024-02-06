89b5df1d6dbaa326302916948fc32cc4f73f40a3
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-06 12:28:07 +0100

    Update crab_MCMinBias_cfg.py

--- CrabSubmission/MC/SkimTau3mu/crab_MCMinBias_cfg.py
+++ CrabSubmission/MC/SkimTau3mu/crab_MCMinBias_cfg.py
@@ -9,7 +9,7 @@ config.General.transferLogs = False
 config.JobType.pluginName = 'Analysis'
 
 
-config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/SkimTools/SkimTau3Mu/test/run_MC2022_PatAndTree_cfg.py'
+config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/SkimTools/SkimTau3Mu/test/run_MCMinBias_PatAndTree_cfg.py'
 config.Data.inputDataset = '/InclusiveDileptonMinBias_TuneCP5Plus_13p6TeV_pythia8/Run3Summer22MiniAODv3-Pilot_124X_mcRun3_2022_realistic_v12-v5/MINIAODSIM'
 config.Data.inputDBS = 'global'
 #config.Data.splitting = 'LumiBased'
