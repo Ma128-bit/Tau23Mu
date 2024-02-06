863d0d360127a09aed1a26385a04f53c18733350
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-06 12:27:29 +0100

    Update crab_MCMinBias_cfg.py

--- CrabSubmission/MC/SkimTau3mu/crab_MCMinBias_cfg.py
+++ CrabSubmission/MC/SkimTau3mu/crab_MCMinBias_cfg.py
@@ -1,1 +1,24 @@
-s
+from CRABClient.UserUtilities import config, getUsername
+config = config()
+
+config.General.requestName = 'SkimTau3mu_MCMinBias_Mini'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+
+
+config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/SkimTools/SkimTau3Mu/test/run_MC2022_PatAndTree_cfg.py'
+config.Data.inputDataset = '/InclusiveDileptonMinBias_TuneCP5Plus_13p6TeV_pythia8/Run3Summer22MiniAODv3-Pilot_124X_mcRun3_2022_realistic_v12-v5/MINIAODSIM'
+config.Data.inputDBS = 'global'
+#config.Data.splitting = 'LumiBased'
+config.Data.splitting = 'Automatic'
+config.Data.unitsPerJob = 1000
+config.Data.outLFNDirBase = '/store/user/mbuonsan/'
+#config.Data.publication = True
+config.Data.outputDatasetTag = 'SkimTau3mu_MCMinBias_Mini'
+config.JobType.allowUndistributedCMSSW = True 
+config.Site.storageSite = 'T2_IT_Bari'
+config.Site.ignoreGlobalBlacklist  = True
+
