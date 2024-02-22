4163d2bf6014c7f76293f0746f0d1d4c2fbe2dd9
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-22 21:54:15 +0100

    Update haddAllJobs_perEra2022.sh

--- Analysis/haddAllJobs_perEra2022.sh
+++ Analysis/haddAllJobs_perEra2022.sh
@@ -40,9 +40,12 @@ hadd JobAdd_perEra/Era_D_tau3mu.root JobAdd_perEra/Era_D-v*_tau3mu.root
 hadd JobAdd_perEra/Era_D_control.root JobAdd_perEra/Era_D-v*_control.root
 rm JobAdd_perEra/Era_D-v*.root
 
-for MC in B0_preE B0_postE Bp_preE Bp_postE DsPhiPi_postE DsPhiPi_preE Ds_preE Ds_postE
-do
-  	echo "Start MC_${MC}!"
-        hadd JobAdd_perEra/MC_${MC}.root ${MC}_tau3mu_${outlable}/AnalysedTree_MC_${MC}_tau3mu*.root
-done
+if [ ${ANALYSISTYPE} == "tau3mu" ]
+then
+	for MC in B0_preE B0_postE Bp_preE Bp_postE DsPhiPi_postE DsPhiPi_preE Ds_preE Ds_postE
+	do
+	  	echo "Start MC_${MC}!"
+	        hadd JobAdd_perEra/MC_${MC}.root ${MC}_tau3mu_${outlable}/AnalysedTree_MC_${MC}_tau3mu*.root
+	done
+ fi
 
