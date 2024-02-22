43b8e6d9aa98bc09f9694945ee20d7ad1f2aa245
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-22 21:58:05 +0100

    Update haddAllJobs_perEra2022.sh

--- Analysis/haddAllJobs_perEra2022.sh
+++ Analysis/haddAllJobs_perEra2022.sh
@@ -36,16 +36,18 @@ do
 	hadd JobAdd_perEra/Era_${era}_${ANALYSISTYPE}.root 2022${era}_*_${ANALYSISTYPE}_${OUT_NAME}/AnalysedTree_${DATATYPE}_2022${era}_*_${ANALYSISTYPE}*.root
 done
 
-hadd JobAdd_perEra/Era_D_tau3mu.root JobAdd_perEra/Era_D-v*_tau3mu.root
-hadd JobAdd_perEra/Era_D_control.root JobAdd_perEra/Era_D-v*_control.root
-rm JobAdd_perEra/Era_D-v*.root
-
 if [ ${ANALYSISTYPE} == "tau3mu" ]
 then
+	hadd JobAdd_perEra/Era_D_tau3mu.root JobAdd_perEra/Era_D-v*_tau3mu.root
+	rm JobAdd_perEra/Era_D-v*_tau3mu.root
 	for MC in B0_preE B0_postE Bp_preE Bp_postE DsPhiPi_postE DsPhiPi_preE Ds_preE Ds_postE
 	do
 	  	echo "Start MC_${MC}!"
 	        hadd JobAdd_perEra/MC_${MC}.root ${MC}_tau3mu_${outlable}/AnalysedTree_MC_${MC}_tau3mu*.root
 	done
- fi
-
+ 
+elif [ ${ANALYSISTYPE} == "control" ]
+then
+	hadd JobAdd_perEra/Era_D_control.root JobAdd_perEra/Era_D-v*_control.root
+	rm JobAdd_perEra/Era_D-v*_control.root
+fi
