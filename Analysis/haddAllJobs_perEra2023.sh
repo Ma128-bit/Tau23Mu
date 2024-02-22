1a0c32ee36ff23ef94f0c7e69b86fe6795d1981b
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-02-22 18:57:25 +0100

    Update haddAllJobs_perEra2023.sh

--- Analysis/haddAllJobs_perEra2023.sh
+++ Analysis/haddAllJobs_perEra2023.sh
@@ -35,3 +35,7 @@ for era in B C-v1 C-v2 C-v3 C-v4 D-v1 D-v2
 do
 	hadd JobAdd_perEra_2023/Era_${era}_${ANALYSISTYPE}.root 2023${era}_*_${ANALYSISTYPE}_${OUT_NAME}/AnalysedTree_${DATATYPE}_2023${era}_*_${ANALYSISTYPE}_merged_${OUT_NAME}.root
 done
+
+hadd JobAdd_perEra_2023/Era_C_tau3mu.root JobAdd_perEra_2023/Era_C-v*_tau3mu.root
+hadd JobAdd_perEra_2023/Era_D_tau3mu.root JobAdd_perEra_2023/Era_D-v*_tau3mu.root
+
