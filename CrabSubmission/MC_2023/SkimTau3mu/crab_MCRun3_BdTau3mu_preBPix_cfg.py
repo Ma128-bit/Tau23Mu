8b81df9fbe3bd8e736867c20b645b8b04f2e2af2
Marco Buonsante <marco.buonsante@cern.ch>
2024-03-12 18:50:09 +0100

    MC23

---
+++ CrabSubmission/MC_2023/SkimTau3mu/crab_MCRun3_BdTau3mu_preBPix_cfg.py
@@ -0,0 +1,24 @@
+from CRABClient.UserUtilities import config, getUsername
+config = config()
+
+config.General.requestName = 'SkimTau3mu_MC_23_Run3_Bd_Mini_preBPix'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+
+
+config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/SkimTools/SkimTau3Mu/test/run_MC2023_PatAndTree_cfg.py'
+config.Data.inputDataset = '/BdtoTau_Tauto3Mu_3MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM'
+config.Data.inputDBS = 'global'
+#config.Data.splitting = 'LumiBased'
+config.Data.splitting = 'Automatic'
+config.Data.unitsPerJob = 2500
+config.Data.outLFNDirBase = '/store/user/mbuonsan/' 
+#config.Data.publication = True
+config.Data.outputDatasetTag = 'SkimTau3mu_MC_23_Run3_Bd_Mini_preBPix'
+config.JobType.allowUndistributedCMSSW = True 
+config.Site.storageSite = 'T2_IT_Bari'
+config.Site.ignoreGlobalBlacklist  = True
+
--- CrabSubmission/MC_2023/SkimTau3mu/submitAllJobs_new.sh
+++ CrabSubmission/MC_2023/SkimTau3mu/submitAllJobs_new.sh
@@ -2,14 +2,14 @@
 # Usage:
 #    reportAllJobs.sh
 
-crab submit -c crab_MCRun3_BdTau3mu_postE_cfg.py
+crab submit -c crab_MCRun3_BdTau3mu_postBPix_cfg.py
 sleep 5
-crab submit -c crab_MCRun3_BdTau3mu_preE_cfg.py
+crab submit -c crab_MCRun3_BdTau3mu_preBPix_cfg.py
 sleep 5
-crab submit -c crab_MCRun3_BuTau3mu_preE_cfg.py
+crab submit -c crab_MCRun3_BuTau3mu_preBPix_cfg.py
 sleep 5
-crab submit -c crab_MCRun3_DsTau3mu_preE_cfg.py
+crab submit -c crab_MCRun3_DsTau3mu_preBPix_cfg.py
 sleep 5
-crab submit -c crab_MCRun3_BuTau3mu_postE_cfg.py
+crab submit -c crab_MCRun3_BuTau3mu_postBPix_cfg.py
 sleep 5
-crab submit -c crab_MCRun3_DsTau3mu_postE_cfg.py
+crab submit -c crab_MCRun3_DsTau3mu_postBPix_cfg.py
