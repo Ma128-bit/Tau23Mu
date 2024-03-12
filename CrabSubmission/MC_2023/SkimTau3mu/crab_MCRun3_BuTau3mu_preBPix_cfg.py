7c529ff2d778cc906eff3f8ce9361b20c20ac4af
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-12 18:58:53 +0100

    Update crab_MCRun3_BuTau3mu_preBPix_cfg.py

--- CrabSubmission/MC_2023/SkimTau3mu/crab_MCRun3_BuTau3mu_preBPix_cfg.py
+++ CrabSubmission/MC_2023/SkimTau3mu/crab_MCRun3_BuTau3mu_preBPix_cfg.py
@@ -10,7 +10,7 @@ config.JobType.pluginName = 'Analysis'
 
 
 config.JobType.psetName = '/lustrehome/mbuonsante/Tau_3mu/Ntuple/CMSSW_13_0_13/src/SkimTools/SkimTau3Mu/test/run_MC2023_PatAndTree_cfg.py'
-config.Data.inputDataset = '/ButoTau_Tauto3Mu_3MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM'
+config.Data.inputDataset = '/ButoTau_Tauto3Mu_3MuFilter_TuneCP5_13p6TeV_pythia8-evtgen/Run3Summer23MiniAODv4-130X_mcRun3_2023_realistic_v14-v2/MINIAODSIM'
 config.Data.inputDBS = 'global'
 #config.Data.splitting = 'LumiBased'
 config.Data.splitting = 'Automatic'
