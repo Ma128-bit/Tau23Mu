2c347ca455cc971f274edbbfe633d793eae1913d
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-03-13 16:54:45 +0100

    Update haddAllJobs_perEra2023.sh

--- Analysis/haddAllJobs_perEra2023.sh
+++ Analysis/haddAllJobs_perEra2023.sh
@@ -28,15 +28,15 @@ then
     DATATYPE='data'
 fi
 
-rm -r JobAdd_perEra_2023
-mkdir JobAdd_perEra_2023
+#rm -r JobAdd_perEra_2023
+#mkdir JobAdd_perEra_2023
 
 for era in B C-v1 C-v2 C-v3 C-v4 D-v1 D-v2
 do
 	hadd JobAdd_perEra_2023/Era_${era}_${ANALYSISTYPE}.root 2023${era}_*_${ANALYSISTYPE}_${OUT_NAME}/AnalysedTree_${DATATYPE}_2023${era}_*_${ANALYSISTYPE}*.root
 done
 
-hadd JobAdd_perEra_2023/Era_C_tau3mu.root JobAdd_perEra_2023/Era_C-v*_tau3mu.root
-hadd JobAdd_perEra_2023/Era_D_tau3mu.root JobAdd_perEra_2023/Era_D-v*_tau3mu.root
-rm JobAdd_perEra_2023/Era_C-v*_tau3mu.root
-rm JobAdd_perEra_2023/Era_D-v*_tau3mu.root
+hadd JobAdd_perEra_2023/Era_C_${ANALYSISTYPE}.root JobAdd_perEra_2023/Era_C-v*_${ANALYSISTYPE}.root
+hadd JobAdd_perEra_2023/Era_D_${ANALYSISTYPE}.root JobAdd_perEra_2023/Era_D-v*_${ANALYSISTYPE}.root
+rm JobAdd_perEra_2023/Era_C-v*_${ANALYSISTYPE}.root
+rm JobAdd_perEra_2023/Era_D-v*_${ANALYSISTYPE}.root
